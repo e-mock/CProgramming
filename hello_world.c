f740fcea998a1d2f87c82e287874ddc207aa4628
e-mock <lizmock02@gmail.com>
2020-05-15 12:05:48 -0500

    added basic functions to main file
    Deleting the semi colon made it so travis catched the build error, so codacy never even saw it.
    Added a dead code function and a add and divide functions I can test with CUnit
    
    Purposely do not check if b is 0 to see if Codacy can flag it as a concern

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+int add(int a, int b){
+   int sum = a + b
+   printf("Added %d and %d to get sum %d", a, b, sum);
+        
+   return sum;
+}
+
+int divide(int a, int b){
+   return a / b;
+}
+
+void deadCodeFunction(){
+   //function that is never called
+   sum = add(1, 3);
+}
+
 int main() {
    printf("Hello World!");
-   printf("Hello again")
+   printf("Hello again");
+   
+   int sum = add(2, 2);
+   
    return 0;
 }
